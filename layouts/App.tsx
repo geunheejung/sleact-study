5770e5f145b76dfe4126e661e4200384eabf5b5f
geunheejung <geunhee0212@gmail.com>
2022-03-26 15:00:35 +0900

    회원가입 페이지 1.

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
+import loadable from '@loadable/component';
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// 알아서 코드스플리팅, 로드해올지 해준다.
+const Login = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => (
-  <div>
-    세팅 그만 할래 ㅠㅠ
-  </div>
+  <Routes>          
+    <Route 
+      path='/'
+      element={<Navigate replace to="/login" />}
+    />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<SignUp />} />
+  </Routes>
 )
 
 export default App;
---
+++ pages/SignUp/index.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useState } from 'react';
+import { Button, Error, Form, Header, Input, Label, LinkContainer, Success } from '@pages/SignUp/styles';
+
+const SignUp = () => {
+  const [mismatchError, setMismatchError] = useState(false);
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value === passwordCheck);                      
+    },
+    [passwordCheck],
+  );
+
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      // 함수 기준으로 외부 변수일 경우 deps에 서술해야한다. 값이 변경되지 않는것은 서술 X
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);      
+    },
+    [password],
+  );
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();    
+      if (mismatchError) return;
+
+      console.log(
+        email, 'email', nickname, 'nickname', password, 'password', mismatchError, 'mismatchError'
+      )
+    },
+    [email, nickname, password, mismatchError],
+  );
+
+  // if (userData) {
+  //   // return <Redirect to="/workspace/sleact" />;  
+  // }
+  
+  console.log(mismatchError)
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {/* {signUpError && <Error>이미 가입된 이메일입니다.</Error>} */}
+          {/* {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>} */}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <a href="/login">로그인 하러가기</a>
+      </LinkContainer>
+    </div>
+  );
+};
+
+export default SignUp;
